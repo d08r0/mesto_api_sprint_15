2b38e735d4da902c0c3c13d13566353eb9a1b42d
scrooge <etoerik@gmail.com>
2020-09-12 13:54:39 +0300

    Add Joi validation for getUsers

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,19 @@ const {
 
 usersRouter.get('/users', getUsers);
 
-usersRouter.get('/users/:id', getUser);
+// usersRouter.get('/users/:id', getUser);
+
+usersRouter.get('/users/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24).hex(),
+  }),
+}), getUser);
 
 usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required(),
   }).unknown(true),
 }), patchUsers);
 
